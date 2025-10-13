42ae2b0ddbd1dca7818453b3e3ddac4fc7698d83
Avinash Ranjan <avinashranjan268@gmail.com>
2025-10-13 20:16:08 +0530

    commit on 13 oct 2025

--- Express/User_Registration_MVC/Controller/controller.js
+++ Express/User_Registration_MVC/Controller/controller.js
@@ -1,49 +1,33 @@
-import {
-    createReadStream
-} from "fs";
-import connectDB from "../config/db.js";
+import { createReadStream } from "fs";
+import { connectDB } from "../config/db.js";
 
-export function displayHomePage(req, res) {
-    res.send(`<h6>Home Page</h6>`);
-}
+export let dHP = (req, res) => {
+  res.send(`<h6>Home Page</h6>`);
+};
 
-export function displayFormPage(req, res) {
-    let formContents = createReadStream("./form.html", "utf-8");
-    formContents.pipe(res);
-}
+export let dFP = (req, res) => {
+  let formContents = createReadStream("./form.html", "utf-8");
+  formContents.pipe(res);
+};
 
-export async function submitForm(req, res) {
-    console.log("user data: " + req.body);
-    let {
-        userEmail,
-        userPassword
-    } = req.body;
-    console.log(req.body); //! whatever data user is submitting, it stores inside req.body which is an object.
-    // { userEmail: 'abc', userPassword: '123' }
-    // let coll = connectDB();
-    // coll.insertOne({ userPassword, userEmail });
+export let submitForm = async (req, res) => {
+  console.log("user data: " + req.body);
+  let { userEmail, userPassword } = req.body;
+  console.log(req.body); //! whatever data user is submitting, it stores inside req.body which is an object.
+  // { userEmail: 'abc', userPassword: '123' }
+  // let coll = connectDB();
+  // coll.insertOne({ userPassword, userEmail });
 
-    let myCollection = await connectDB();
-    // myCollection.insertOne({ userEmail: userEmail, userPassword: userPassword });
-    let op = await myCollection.insertOne({
-        userEmail,
-        userPassword
-    });
+  let myCollection = await connectDB();
+  // myCollection.insertOne({ userEmail: userEmail, userPassword: userPassword });
+  let op = await myCollection.insertOne({ userEmail, userPassword });
 
-    res.json({
-        success: true,
-        message: "user registered successfully",
-        op
-    });
-}
+  res.json({ success: true, message: "user registered successfully", op });
+};
 
-export async function getAllUSers(req, res) {
-    let myCollection = await connectDB();
-    let users = await myCollection.find().toArray();
-    let nameArr = users.map((user) => user.userEmail);
-    res.json({
-        success: true,
-        message: "users fetched",
-        nameArr
-    });
-}
+export let allUSers = async (req, res) => {
+  let myCollection = await connectDB();
+  let users = await myCollection.find().toArray();
+  let nameArr = users.map((user) => user.userEmail);
+  res.json({ success: true, message: "users fetched", nameArr });
+};
--- Express/User_Registration_MVC/app.js
+++ Express/User_Registration_MVC/app.js
@@ -1,27 +1,22 @@
 import express from "express";
-import connectDB from './config/db.js';
+import connectDB from "./config/db.js";
 import fs from "fs";
-import routesFile from './Routes/routes.js';
-
-
+import routesFile from "./Routes/routes.js";
 
 let app = express();
 
-app.use(express.urlencoded({
-    extended: true // middleware it parse/read the incoming html form data
-}))
+app.use(
+  express.urlencoded({
+    extended: true, // middleware it parse/read the incoming html form data
+  })
+);
 app.use("/api/v1", routesFile);
 //! API versionaing, static path
 
-
 app.listen(9000, (err) => {
-    if (err)
-        console.log(err);
-    console.log("Server started");
+  if (err) console.log(err);
+  console.log("Server started");
 });
-
-
-
 
 //! home page
 // app.get("/", (req, res) => {
@@ -30,55 +25,52 @@ app.listen(9000, (err) => {
 
 //! form page
 app.get("/get-form", (req, res) => {
-    let formContents = fs.createReadStream("./form.html", "utf-8")
-    formContents.pipe(res);
-})
+  let formContents = fs.createReadStream("./form.html", "utf-8");
+  formContents.pipe(res);
+});
 
 //! form sunmit
 //? in the HTML form
 //? a) give value to action attribute which should same as the endpoint.
 //?  b) set attribute method with its value as post.
-//? c) use name attribute to give variable name 
+//? c) use name attribute to give variable name
 app.post("/submit-form", async (req, res) => {
-    console.log("user data: " + req.body);
-    let {
-        userEmail,
-        userPassword
-    } = req.body;
-    console.log(req.body); //! whatever data user is submitting, it stores inside req.body which is an object.
-    // { userEmail: 'abc', userPassword: '123' }
-    // let coll = connectDB();
-    // coll.insertOne({ userPassword, userEmail });
+  console.log("user data: " + req.body);
+  let { userEmail, userPassword } = req.body;
+  console.log(req.body); //! whatever data user is submitting, it stores inside req.body which is an object.
+  // { userEmail: 'abc', userPassword: '123' }
+  // let coll = connectDB();
+  // coll.insertOne({ userPassword, userEmail });
 
-    let myCollection = await connectDB();
-    // myCollection.insertOne({ userEmail: userEmail, userPassword: userPassword });
-    let op = await myCollection.insertOne({
-        userEmail,
-        userPassword
-    });
+  let myCollection = await connectDB();
+  // myCollection.insertOne({ userEmail: userEmail, userPassword: userPassword });
+  let op = await myCollection.insertOne({
+    userEmail,
+    userPassword,
+  });
 
-    res.json({
-        success: true,
-        message: "user registered successfully",
-        op
-    });
+  res.json({
+    success: true,
+    message: "user registered successfully",
+    op,
+  });
 });
 
 //! list
 app.get("/all-users", async (req, res) => {
-    let myCollection = await connectDB();
-    let users = await myCollection.find().toArray();
-    let nameArr = users.map((user) => user.userEmail);
-    res.json({
-        success: true,
-        message: "users fetched",
-        nameArr
-    });
+  let myCollection = await connectDB();
+  let users = await myCollection.find().toArray();
+  let nameArr = users.map((user) => user.userEmail);
+  res.json({
+    success: true,
+    message: "users fetched",
+    nameArr,
+  });
 });
 
 app.listen(9001, (err) => {
-    if (err) console.log(err);
-    console.log("server running");
+  if (err) console.log(err);
+  console.log("server running ay 9000");
 });
 
 //? nodemon -v
